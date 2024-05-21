b0cdb8a4f08bcdbc1179b21b79735e469c189006
Oliver Tworkowski <olitworkowski@gmail.com>
2024-05-21 21:46:19 +0200

    implement pair receival from brute force

---
+++ hloc/pairs_from_all.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+from . import logger
+from .utils.parsers import parse_image_list_dir
+
+
+def main(
+    output: Path,
+    image_list: Path,
+):
+    if image_list is not None:
+        names_q = parse_image_list_dir(image_list)
+    else:
+        raise ValueError(f"Unknown type for image list: {image_list}")
+
+    sorted_image_ids = sorted(names_q)
+    pairs = []
+    for i in range(len(sorted_image_ids)):
+        for j in range(i + 1, len(sorted_image_ids)):
+            pairs.append((sorted_image_ids[i], sorted_image_ids[j]))
+
+    logger.info(f"Found {len(pairs)} pairs.")
+    print(pairs[0])
+    with open(output, "w") as f:
+        f.write("\n".join(" ".join([i, j]) for i, j in pairs))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", required=True, type=Path)
+    parser.add_argument("--image_list", required=True, type=Path)
+    args = parser.parse_args()
+    main(**args.__dict__)
