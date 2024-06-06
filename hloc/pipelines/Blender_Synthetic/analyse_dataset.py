implement more reliable helmert transform based on averages, fix some bugs in the pipeline comparator and notebooks

hloc/pipelines/Blender_Synthetic/analyse_dataset.py
@@ -1,13 +1,9 @@
 import json
 import bpy
 import sys
-from random import uniform
-import time
 import numpy as np
 import re
-from pprint import pp
-from mathutils import Matrix, Euler, Quaternion, Vector
-import bmesh
+from mathutils import Matrix
 import argparse
 from pathlib import Path
 
@@ -16,7 +12,7 @@ transforms = None
 DEFAULT_FOCAL_LENGTH = 27
 DEFAULT_SENSOR_WIDTH = 35
 MAX_REPROJECTION_ERROR_THESHOLD = 1.0  # warning if avg is above
-MAX_CAMS = 3  # for transformation alignment
+MAX_CAMS = 10  # for transformation alignment
 
 
 def eval_args():
@@ -44,57 +40,76 @@ def get_cameras(match_name=None, parent=None):
     return cams
 
 
+def is_main_cam(cam_map_item):
+    return "MAIN" in cam_map_item["c1"].name or "MAIN" in cam_map_item["c2"].name
+
 # m2 is subset of m1 names! m1=GT, m2=SYNTH
 def map_cameras(m1, m2, data):
     m2_names = [c.name.split(".")[0] for c in m2]
     m1_names = [c.name.split(".")[0] for c in m1]
     diff = set(m1_names) - set(m2_names)
     if len(diff) > 0:
-        raise Exception(f"These cameras are missing in the estimated camera poses! {diff}")
+        print(f"These cameras are missing in the estimated camera poses! {diff}")
+        #if "MAIN" in diff: # improve this! should be LIKE and not hard match
+        #    raise Exception(f"These cameras are missing in the estimated camera poses! {diff}")
 
     map = []
     for c1 in m1:
         for c2 in m2:
             if c1.name in c2.name or ("MAIN" in c1.name and "MAIN" in c2.name):
-                print(f"INFO: matched {c1.name} and {c2.name}")
+                #print(f"INFO: matched {c1.name} and {c2.name}")
                 # append error
+                mapped_item = None
                 for frame in data["frames"]:
                     f_name = frame["file_path"]
                     if f_name == c2.name:
-                        print(f"INFO: added error metric for {c2.name} from frame")
-                        map.append({"c1": c1, "c2": c2, "error": frame["error"]})
+                        #print(f"INFO: added error metric for {c2.name} from frame")
+                        mapped_item = {"c1": c1, "c2": c2, "error": frame["error"]}
                         break
+                if not mapped_item:
+                    # in case error not found, just add a large error
+                    mapped_item = {"c1": c1, "c2": c2, "error": 1337}
+                map.append(mapped_item)
                 break
-    assert len(m1) == len(map)
+    #assert len(m1) == len(map), "gt poses and est poses could not be mapped"
+
+    missing_main_cam = True
+    for e in map:
+        if is_main_cam(e):
+            missing_main_cam = False
+            break
+
+    if missing_main_cam:
+        raise Exception(f"Main camera could not be found in localization")
+    
     return map
 
 
 def get_top_cams(cam_map):
     sorted_cams = sorted(cam_map, key=lambda x: x["error"])
-    if len(cam_map) < MAX_CAMS:
-        print(
-            "cannot select top cameras for optimal alignment. There need to be at least N cameras available!"
-        )
-    else:
-        sorted_cams = sorted_cams[:MAX_CAMS]
-    mean_error = np.asarray([x["error"] for x in sorted_cams]).mean()
+    sorted_cams = [i for i in sorted_cams if not is_main_cam(i)] # filter out main cam
+    
+    filtered_cams = []
+    max_error_threshold = MAX_REPROJECTION_ERROR_THESHOLD
+    it_increase = 0.05
+    it = 1
+    # try to find at least MAX_CAMS which have low reprojection error
+    while True:
+        filtered_cams = [c for c in sorted_cams if c["error"] < max_error_threshold]
+        if len(filtered_cams) >= MAX_CAMS:
+            break
+        it += 1
+        max_error_threshold += it_increase
+
+    mean_error = np.asarray([x["error"] for x in filtered_cams]).mean()
+    print(f"INFO: found {len(filtered_cams)} with mean error of {mean_error} after {it} iterations with an increase in error of {max_error_threshold - MAX_REPROJECTION_ERROR_THESHOLD}px")
+        
     if mean_error > MAX_REPROJECTION_ERROR_THESHOLD:
         print(
             f"WARNING: camera exceeds max error theshold and could lead to inaccurately aligned cameras: {mean_error}"
         )
 
-    c1c2_d = (sorted_cams[0]["c2"].location - sorted_cams[1]["c2"].location).length
-    c1c3_d = (sorted_cams[0]["c2"].location - sorted_cams[2]["c2"].location).length
-    if c1c3_d > c1c2_d:
-        # switch sorted cams location so that the second cam is further away
-        # further distance = finer scaling
-        sorted_cams[1], sorted_cams[2] = sorted_cams[2], sorted_cams[1]
-
-    # 1: set position of first cam to GT first cam
-    # 2: scale to fit 2nd cam with GT 2nd cam
-    # 3: rotate to fit 2nd cam with GT 2nd cam
-    # 4: rotate to fit 3rd cam with GT 3rd cam
-    return sorted_cams
+    return filtered_cams
 
 
 def helmert_transform(ground_truth_points, estimated_points):
@@ -121,23 +136,44 @@ def helmert_transform(ground_truth_points, estimated_points):
     #print("Translation:", translation)
 
     return scale, rotation, translation
+    
+def helmert_transform_averaged(ground_truth_points, estimated_points):
+    # Compute centroids
+    centroid_gt = np.mean(ground_truth_points, axis=0)
+    centroid_est = np.mean(estimated_points, axis=0)
+    #print("Ground Truth Centroid:", centroid_gt)
+    #print("Estimated Points Centroid:", centroid_est)
+
+    # Center the points around the centroids
+    centered_gt = ground_truth_points - centroid_gt
+    centered_est = estimated_points - centroid_est
+
+    # Compute scale
+    scale = np.sqrt(np.sum(centered_gt**2) / np.sum(centered_est**2))
+    #print("Scale:", scale)
+
+    # Compute rotation using SVD
+    H = np.dot(centered_est.T, centered_gt)
+    U, _, Vt = np.linalg.svd(H)
+    rotation = np.dot(Vt.T, U.T)
+    #print("Initial Rotation Matrix:\n", rotation)
+
+    # Ensure a proper rotation matrix (det(R) = 1)
+    if np.linalg.det(rotation) < 0:
+        Vt[-1, :] *= -1
+        rotation = np.dot(Vt.T, U.T)
+    #print("Adjusted Rotation Matrix:\n", rotation)
+
+    # Compute translation
+    translation = centroid_gt - scale * np.dot(rotation, centroid_est)
+    #print("Translation:", translation)
+
+    return scale, rotation, translation
 
 
 def apply_helmert_transform(points, scale, rotation, translation):
     transformed_points = scale * np.dot(points, rotation.T) + translation
     return transformed_points
-
-
-def apply_helmert_transform_blender(cam, scale, rotation, translation):
-    # Apply the scale, rotation, and translation to the camera location using Blender's API
-    est_position = np.array(cam.location)
-    transformed_position = scale * np.dot(rotation, est_position) + translation
-    cam.location = transformed_position
-
-    # Convert the rotation matrix to Euler angles and set the rotation
-    rotation_matrix = Matrix(rotation).transposed()  # Transpose the rotation matrix
-    rotation_euler = rotation_matrix.to_euler()
-    cam.rotation_euler = rotation_euler
 
 
 def align_estimated_to_ground_truth(est_cams, top_cams, apply_to=None):
@@ -146,7 +182,7 @@ def align_estimated_to_ground_truth(est_cams, top_cams, apply_to=None):
     est_positions = np.array([cam["c2"].location for cam in top_cams])
 
     # Compute the Helmert transformation
-    scale, rotation, translation = helmert_transform(gt_positions, est_positions)
+    scale, rotation, translation = helmert_transform_averaged(gt_positions, est_positions)
 
     try:
         local_rot = Matrix(np.linalg.inv(rotation)).transposed().to_euler()
@@ -169,7 +205,7 @@ def align_estimated_to_ground_truth(est_cams, top_cams, apply_to=None):
         # apply local rotation by calculating the inverse of the rotation matrix
         cam.rotation_euler.rotate(local_rot)
 
-    print("Alignment complete")
+    #print("Alignment complete")
 
 
 def analyse_poses(cam_map):

hloc/pipelines/Blender_Synthetic/pipeline.py
@@ -13,9 +13,14 @@ from hloc import (
     pairs_from_all,
     triangulation,
     reconstruction,
+    pairs_from_exhaustive,
 )
 
+from hloc.utils.parsers import parse_image_list_dir
 from third_party.Neuralangelo.convert_data_to_json import data_to_json
+import pycolmap
+from hloc.localize_sfm import QueryLocalizer, pose_from_cluster
+import numpy as np
 
 # from hloc.pipelines.Blender_Synthetic import analyse_dataset
 
@@ -43,18 +48,26 @@ config = [
         "matcher": "superpoint+lightglue",
         "name": "superpoint+lightglue",
     },
-    #{"extractor": "s2dnet", "matcher": "loftr", "name": "..."},  # TODO: add s2d
-    #{"extractor": "r2d2", "matcher": "loftr", "name": "r2d2+loftr"},  # TODO: add r2d2
+    {"extractor": "r2d2", "matcher": "NN-ratio", "name": "r2d2+nn_ratio"},
+    #{
+    #    "extractor": "s2dnet",
+    #    "matcher": "NN-ratio",
+    #    "name": "s2d+nn_ratio",
+    #},  # TODO: add s2d
     # TODO: add HP (Does not work...)
 ]
 
-config1 = [
+"""
+config = [
     {
         "extractor": "superpoint_max",
         "matcher": "superglue",
         "name": "superpoint+superglue",
     },
 ]
+
+_datasets = [("framlingham2", "datasets/framlingham2/framlingham.blend")]
+"""
 
 _datasets = [
     (
@@ -80,7 +93,7 @@ _datasets = [
     (
         "Pelegrina+evening_field_8k",
         "datasets/Pelegrina+evening_field_8k/pelegrina.blend",
-    )
+    ),
 ]
 
 
@@ -102,6 +115,10 @@ def get_matcher_config(key):
     return (config, is_dense)
 
 
+def is_loc_dataset(ds: Path):
+    return (ds / "images" / "query").exists()
+
+
 def validate():
     required_keys = ["matcher", "name"]
     for c in config:
@@ -120,6 +137,23 @@ def component_id(component: dict, dataset_name: str):
     return f'{dataset_name}-{component["name"]}'
 
 
+# temporary construct for images processed in transform file generator
+class TempImage:
+    def __init__(self, qvec, tvec, name, point3d_ids=None):
+        self.qvec = np.array(qvec)
+        self.tvec = np.array(tvec)
+        self.name = name
+        if point3d_ids:
+            self.point3D_ids = point3d_ids
+
+    def todict(self):
+        return {
+            "qvec": self.qvec,
+            "tvec": self.tvec,
+            "name": self.name,
+        }
+
+
 # runs selected component on dataset
 def run_component(component, dataset) -> Path:
     id = component_id(component, dataset)
@@ -127,6 +161,8 @@ def run_component(component, dataset) -> Path:
     if OUT.exists():
         log.info(f"  SKIPPING Component - '{id}' already ran for dataset '{dataset}'")
         return None
+
+    log.info(f"  RUNNING Component - '{id}' for dataset '{dataset}'")
 
     OUT.mkdir(parents=True, exist_ok=True)
 
@@ -139,7 +175,9 @@ def run_component(component, dataset) -> Path:
     sfm_dir = OUT / id
 
     # get image pairs
-    pairs_from_all.main(sfm_pairs, IMAGES)
+    pairs_from_all.main(
+        sfm_pairs, IMAGES, recursive=False
+    )  # IMPORTANT: non recursive to only load first layer
 
     feature_path = None
     # extract features
@@ -166,15 +204,70 @@ def run_component(component, dataset) -> Path:
     # start reconstruction
     model = reconstruction.main(sfm_dir, IMAGES, sfm_pairs, feature_path, match_path)
 
-    # TODO: localization pipeline?
-
-    # export transforms and copy images
+    # export transforms args
     args = {
         "data_dir": str(sfm_dir.resolve()),
         "scene_type": "outdoor",
         "image_dir": str(IMAGES.resolve()),
         "name": "transforms.json",
     }
+
+    # TODO: localization pipeline?
+    if is_loc_dataset(IN):
+        # apply localization
+        QUERY_IMAGE_PATH = IMAGES / "query"
+        QUERY_IMAGE = parse_image_list_dir(QUERY_IMAGE_PATH, recursive=False)
+        if len(QUERY_IMAGE) == 0:
+            raise Exception("no query images found")
+        QUERY_IMAGE = f"query/{QUERY_IMAGE[0]}"
+
+        references_registered = [model.images[i].name for i in model.reg_image_ids()]
+
+        # extract features and match query image again
+        extract_features.main(
+            extractor_conf,
+            IMAGES,
+            image_list=[QUERY_IMAGE],
+            feature_path=feature_path,
+            overwrite=True,
+        )
+        pairs_from_exhaustive.main(
+            sfm_pairs, image_list=[QUERY_IMAGE], ref_list=references_registered
+        )
+        match_features.main(
+            matcher_conf,
+            sfm_pairs,
+            features=feature_path,
+            matches=match_path,
+            overwrite=True,
+        )
+
+        # register new camera and localize it
+        camera = pycolmap.infer_camera_from_image(IMAGES / QUERY_IMAGE)
+        ref_ids = [
+            model.find_image_with_name(n).image_id for n in references_registered
+        ]
+        conf = {
+            "estimation": {"ransac": {"max_error": 12}},
+            "refinement": {"refine_focal_length": True, "refine_extra_params": True},
+        }
+        localizer = QueryLocalizer(model, conf)
+        ret, output_log = pose_from_cluster(
+            localizer, QUERY_IMAGE, camera, ref_ids, feature_path, match_path
+        )
+
+        # extract localized camera
+        localized_image = TempImage(
+            ret["cam_from_world"].rotation.todict()[
+                "quat"
+            ],  # do this to as a workaround to extract numpy array instead of Rotation3D quaternion
+            ret["cam_from_world"].translation,
+            QUERY_IMAGE,
+            None,  # [i + 1 if e else -1 for i, e in enumerate(ret["inliers"])],
+        )
+        args["extra_cam"] = localized_image
+
+    # export transforms and copy images
     data_to_json(args)
 
     sfm_images = sfm_dir / IMAGES_FOLDER_NAME
@@ -188,6 +281,13 @@ def run_component(component, dataset) -> Path:
 
 
 def run_analysis(blender_file: Path, transforms_json: Path, images_path: Path):
+    if not blender_file.exists():
+        raise Exception("Blender file missing")
+    if not transforms_json.exists():
+        raise Exception("Transforms file missing")
+    if not images_path.exists():
+        raise Exception("Images path missing")
+
     subprocess.run(
         [
             "blender",
@@ -226,7 +326,10 @@ def main(args):
     log.info("Step 1: reconstructions")
     for c in config:
         # implement timer!
-        run_component(c, dataset_name)
+        try:
+            run_component(c, dataset_name)
+        except Exception as E:
+            log.error(f"  There was a problem with the component run: {E}")
 
     log.info("Step 2: analysis")
     for c in config:
@@ -234,11 +337,16 @@ def main(args):
         out = ROOT / "out" / dataset_name / id / id
         transforms_json = Path(out / "transforms.json").resolve()
         stats_json = Path(out / "stats.json").resolve()  # saved stats
-        if stats_json.exists():
+        if stats_json.exists() and not args.overwrite_analysis:
             log.info(f"  SKIPPING Analysis - {id}")
             continue
+
+        log.info(f"  RUNNING Analysis - {id}")
         images = Path(out / "images").resolve()
-        run_analysis(blender_file, transforms_json, images)
+        try:
+            run_analysis(blender_file, transforms_json, images)
+        except Exception as E:
+            log.error(f"  There was a problem with the component analysis: {E}")
     return
 
 
@@ -255,6 +363,13 @@ if __name__ == "__main__":
         type=Path,
         required=False,
         help="Blender validation file path",
+    )
+    parser.add_argument(
+        "--overwrite_analysis",
+        type=bool,
+        required=False,
+        help="Overwrite Analysis Results",
+        default=False
     )
     args = parser.parse_args()
     if not args.dataset or not args.validator: